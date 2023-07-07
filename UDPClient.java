b8e74ab3d26e2bab6ac6bb614dacb85a29bd7b70
Arpan Srivastava <azs0239@auburn.edu>
2023-07-07 01:56:15 -0500

    complete

---
+++ UDPClient.java
@@ -0,0 +1,165 @@
+import java.io.*;
+import java.net.*;
+import java.lang.Math;
+import java.util.Random;
+import java.util.ArrayList;
+
+/*
+    Authors: Arpan Srivastava
+             Joe Szempruch
+             Colby Deason
+*/
+
+class UDPClient {
+    public static void main(String[] args) throws Exception {
+       
+        //variables
+        byte[] sendData = new byte[1024];
+        byte [] receiveData = new byte[1024];
+        String request = "";
+        String[] checkup;
+        String modifiedSentence = "";
+
+        //creates input stream
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        InetAddress IPAddress = null;
+
+        //creates client socket
+        DatagramSocket clientSocket = new DatagramSocket();
+
+
+        //Prompts the user for IP address of the server
+        System.out.println("Enter server IPAddress to connect to:");
+        String ip = inFromUser.readLine();
+        try {
+            //Translate hostname to IP address
+            IPAddress = InetAddress.getByName(ip);
+        } catch (UnknownHostException e) {
+            System.out.println("invalid IPAddress! Please enter a valid IP.");
+        }
+
+
+        //prompts user for the gremlin probability
+        System.out.println("Enter your gremlin probability: ");
+        double gremlinProb = Double.parseDouble(inFromUser.readLine());
+
+
+       //prompts user for the HTTP request
+        do {
+        System.out.println("Enter an HTTP GET request.");
+        request = inFromUser.readLine();
+        checkup = request.split(" ");
+        } while (checkup.length != 3);
+
+        //converting the string into a byte array
+        sendData = request.getBytes();
+
+
+        
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 10005);
+
+        //sending datagram to the server
+        clientSocket.send(sendPacket);
+
+
+        //Read the datagram coming from server until nullbyte packet is received
+        boolean finished =false;
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        int count = 0;
+        ArrayList<Integer> corruptedPackets = new ArrayList<>();
+
+        while(finished == false) {
+        clientSocket.receive(receivePacket);
+        if (receivePacket.getData()[0] == (byte) '\u0000')
+            break;
+        
+
+        if (count!=0) {
+        modifiedSentence = new String(receivePacket.getData());
+        System.out.print(modifiedSentence);
+        }
+
+        if (count > 1) {
+            byte[] uncorrupted = receivePacket.getData();
+            int uncorruptedChecksum = calcChecksum(uncorrupted);
+   
+            byte[] corrupted = Gremlin(gremlinProb, uncorrupted);
+            int corruptedCheckSum = calcChecksum(corrupted);
+
+            if(corruptedCheckSum != uncorruptedChecksum) {
+                int num = count - 2;
+                corruptedPackets.add(num);
+            }
+
+        }
+
+        count++;
+    } 
+
+        System.out.println("\n-----------------------------------\nAll Packets Received\n-----------------------------------");
+        if (corruptedPackets.isEmpty()) {
+            System.out.println("No packets were corrupted");
+        } else {
+            System.out.println("The following packets were corrupted: \n" + corruptedPackets);
+        }
+        System.out.println("-----------------------------------");
+
+
+        
+
+        
+        clientSocket.close();
+    }
+
+
+    //Gremlin function as defined in the pdf
+    public static byte[] Gremlin(double p, byte[] pack) {
+        if (Math.random() < p) {
+            double x = Math.random();
+            //1 byte corrupted
+            if (x <= 0.5) {
+                Random rand = new Random();
+                int z = rand.nextInt(1024);
+                pack[z] /= 2;
+            }
+            //2 bytes corrupted
+            else if (x <= 0.8) {
+                Random rand = new Random();
+                int z = rand.nextInt(1024);
+                int y = rand.nextInt(1024);
+                if (x == y) {
+                    y = rand.nextInt(1024);
+                }
+                pack[z] /= 2;
+                pack[y] /= 2;
+            }
+            //3 bytes corrupted
+            else {
+                Random rand = new Random();
+                int z = rand.nextInt(1024);
+                int y = rand.nextInt(1024);
+                int q = rand.nextInt(1024);
+                if (x == y || x == q || y == q) {
+                    y = rand.nextInt(1024);
+                    q = rand.nextInt(1024);
+                }
+                pack[z] /= 2;
+                pack[y] /= 2;
+                pack[q] /= 2;
+            }
+        }
+        return pack;
+    }
+
+
+
+    /*Calculates the checksum*/
+    public static int calcChecksum(byte[] data) {
+        int calc_checksum = 0;
+        for (int i = 0; i < data.length; i++) {
+            calc_checksum += data[i];
+        }
+        return calc_checksum;
+    }
+
+}
---
+++ UDPServer.java
@@ -0,0 +1,149 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.file.*;
+/*
+    Authors: Arpan Srivastava
+             Joe Szempruch
+             Colby Deason
+*/
+
+class UDPServer {
+    public static void main(String[] args) throws Exception {
+        //Create datagram socket at port 10005
+        DatagramSocket serverSocket = new DatagramSocket(10005);
+
+        byte[] receiveData = new byte[1024];
+        String nullByte = "\0";
+        String errorCode = "";
+
+        while(true) {
+            //Receiving from client
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+
+            String sentence = new String(receivePacket.getData());
+            System.out.println("Data received is: " + sentence);
+
+            
+            String[] arr = sentence.split(" ");
+            String fileNameRequested = arr[1];
+
+            //creating bufferedReader to read in the file 
+    
+            try {
+            BufferedReader fileIn = new BufferedReader(new FileReader(fileNameRequested));
+            StringBuilder fileData = new StringBuilder();
+            String temp = fileIn.readLine();
+            System.out.println("line: " + temp);
+            while (temp != null) {
+                System.out.println(temp);
+                fileData.append(temp);
+                temp = fileIn.readLine();
+            }
+             fileIn.close();
+        } catch(FileNotFoundException e) { 
+                // catch exception if file is not found, send error html file. 
+                System.err.println("File not found. Program exiting. ");
+                errorCode = "404 - File Not Found";
+            }
+            
+
+            if (errorCode.isEmpty()) {
+                errorCode = "200 - Document follows";
+            }
+
+            
+            byte[] bytes = null;
+            try {
+                bytes = Files.readAllBytes(Paths.get(fileNameRequested));
+            } catch(NoSuchFileException e) {
+                bytes = Files.readAllBytes(Paths.get("errorFile.html"));
+            }
+            
+            // HTTPHeader:
+            String HTTPHeader = "\n" + arr[2] + " " + errorCode + "\r\nContent-Type: text/plain\r\nContent-Length: " + bytes.length + " bytes\r\n";
+           
+            byte[] headerPacket = HTTPHeader.getBytes();
+            //creates a datagram with data to send, length, IP addr, port no.
+
+            DatagramPacket sendedPacket = new DatagramPacket(headerPacket, headerPacket.length, IPAddress, port);
+            
+            serverSocket.send(sendedPacket); 
+
+            boolean finished = false;
+            int start = 0;
+            int end = 1024;
+            if (end > headerPacket.length) {
+                end = headerPacket.length;
+            }
+
+            while (finished == false) {
+                byte[] temp = new byte[1024];
+                int dataIndex = 0;
+                for(int i = start; i < end; i++) {
+                
+                    temp[dataIndex] = headerPacket[i];
+                    dataIndex++;
+                    if(i == headerPacket.length - 1) {
+                        finished = true;
+                    }
+                }
+                start = end;
+                if (end + 1024 < headerPacket.length) {
+                    end+= 1024;
+                } else {
+                    end = headerPacket.length;
+                }
+            
+            DatagramPacket sendPacket = new DatagramPacket(temp, temp.length, IPAddress, port);
+            
+            serverSocket.send(sendPacket); 
+            }
+
+
+            finished = false;
+            start = 0;
+            end = 1024;
+            if (end > bytes.length) {
+                end = bytes.length;
+            }
+
+            while (finished == false) {
+                byte[] data = new byte[1024];
+                int dataIndex = 0;
+                for(int i = start; i < end; i++) {
+                
+                    data[dataIndex] = bytes[i];
+                    dataIndex++;
+                    if(i == bytes.length - 1) {
+                        finished = true;
+                    }
+                }
+                start = end;
+                if (end + 1024 < bytes.length) {
+                    end+= 1024;
+                } else {
+                    end = bytes.length;
+                }
+
+            
+            DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, port);
+            
+            serverSocket.send(sendPacket); 
+            }
+
+            //Sending last nullbyte packet
+            byte[] nullBytePacket = nullByte.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(nullBytePacket, nullBytePacket.length, IPAddress, port);
+            serverSocket.send(sendPacket); 
+            System.out.println("----------------------------------\nnullByte packet sent.\n----------------------------------");
+            // end program
+            System.exit(0); 
+        }
+    }
+}
